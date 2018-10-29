23ecac476376ea78d2c458fd7137ba68219fdaa3
Umidbek Karimov <uma.karimov@gmail.com>
2018-10-29 18:49:17 +0500

    feat(babel-preset): Add `test` mode.

--- src/babel-preset.ts
+++ src/babel-preset.ts
@@ -4,14 +4,21 @@ type BabelDependency<T = object> = [string, T];
 
 interface Options {
   target: "web" | "node";
-  mode: "development" | "production";
+  mode: "test" | "development" | "production";
 }
 
 const targets = new Set(["web", "node"]);
-const modes = new Set(["development", "production"]);
+const modes = new Set(["test", "development", "production"]);
+
+const optionalDependencies = new Set(["@babel/preset-typescript"]);
 
 function createError(message: string) {
-  return new Error(`[pack-a-tron/babel] ERROR: ${message}`);
+  const error = new Error(`[pack-a-tron/babel] ERROR: ${message}`);
+
+  // @ts-ignore
+  error.framesToPop = 1;
+
+  return error;
 }
 
 function parseOptions(options: unknown): Options {
@@ -24,18 +31,26 @@ function parseOptions(options: unknown): Options {
   const { mode, target, ...unknownOptions } = options as Partial<Options>;
 
   if (!mode || !modes.has(mode)) {
-    throw createError(`Unknown mode: "${mode}".`);
+    throw createError(
+      `Unknown mode: "${mode}". Expected one of: ${Array.from(modes).join(
+        ", ",
+      )}.`,
+    );
   }
 
   if (!target || !targets.has(target)) {
-    throw createError(`Unknown target: "${mode}".`);
+    throw createError(
+      `Unknown target: "${target}". Expected one of: ${Array.from(targets).join(
+        ", ",
+      )}.`,
+    );
   }
 
   const unknownKeys = Object.keys(unknownOptions);
 
   if (unknownKeys.length > 0) {
     throw createError(
-      `${unknownKeys.length} unknown option(s):\n${unknownKeys.join(", ")}.`,
+      `${unknownKeys.length} unknown option(s): ${unknownKeys.join(", ")}.`,
     );
   }
 
@@ -52,35 +67,51 @@ function warning(message: string, ...args: unknown[]): void {
   console.error(message, ...args);
 }
 
-function resolveDependencies(dependencies: BabelDependency[]) {
+function resolveDependencies(dependencies: Array<null | BabelDependency>) {
   const unresolved: string[] = [];
   const resolved: BabelDependency[] = [];
 
-  dependencies.forEach(([id, options]) => {
+  dependencies.forEach(dependency => {
+    if (!dependency) {
+      return;
+    }
+
+    const [id, options] = dependency;
+
     const idPath = tryResolve(id);
 
     if (idPath) {
       resolved.push([idPath, options]);
-    } else {
+    } else if (!optionalDependencies.has(id)) {
       unresolved.push(id);
     }
   });
 
   if (unresolved.length > 0) {
-    warning("ERROR: Install:\n`%s`.", unresolved.join(" "));
+    warning("ERROR: Install: `%s`.", unresolved.join(" "));
   }
 
   return resolved;
 }
 
-// eslint-disable-next-line typescript/no-explicit-any
-export default function declare(api: any, options: unknown): BabelConfig {
+interface BabelApi {
+  assertVersion(version: number): void;
+}
+
+export default function declare(api: BabelApi, options: unknown): BabelConfig {
   api.assertVersion(7);
 
   const { mode, target } = parseOptions(options);
 
+  const isWeb = target === "web";
+
+  const isDev = mode === "development";
+  const isTest = mode === "test";
+
   return {
     presets: resolveDependencies([
+      ["@babel/preset-typescript", {}],
+
       [
         "@babel/preset-env",
         {
@@ -91,18 +122,14 @@ export default function declare(api: any, options: unknown): BabelConfig {
           // bundle size. We shouldn't rely on magic to try and shrink it.
           useBuiltIns: false,
 
-          // Do not transform modules to CJS.
-          modules: false,
+          // Transform modules only in test environment.
+          modules: isTest && "commonjs",
 
           // Exclude transforms that make all code slower.
           exclude: ["transform-typeof-symbol"],
 
-          targets:
-            target === "node"
-              ? // We want to support LTS Node Version.
-                { node: "8.0.0" }
-              : // We want to support IE 9.
-                { ie: 9 },
+          // Support IE 9 on web or NodeJS 8.0.0.
+          targets: isWeb ? { ie: 9 } : { node: isTest ? true : "8.0.0" },
         },
       ],
 
@@ -111,7 +138,7 @@ export default function declare(api: any, options: unknown): BabelConfig {
         {
           // Adds component stack to warning messages
           // Adds __self attribute to JSX which React will use for some warnings
-          development: mode === "development",
+          development: isDev,
 
           // Will use the native built-in instead of trying to polyfill
           // behavior for any plugins that require one.
@@ -121,49 +148,44 @@ export default function declare(api: any, options: unknown): BabelConfig {
     ]),
 
     plugins: resolveDependencies([
-      // `await import('./big-module')`
       ["@babel/plugin-syntax-dynamic-import", {}],
 
       [
-        // Necessary to include regardless of the environment because
-        // in practice some other transforms (such as object-rest-spread)
-        // don't work without it: https://github.com/babel/babel/issues/7215
         "@babel/plugin-transform-destructuring",
-
-        // Use `Object.assign`.
-        { useBuiltIns: true },
+        { useBuiltIns: true }, // Use `Object.assign`.
       ],
 
+      ["@babel/plugin-proposal-export-default-from", {}],
+
       [
-        // See discussion in https://github.com/facebook/create-react-app/issues/4263
         "@babel/plugin-proposal-class-properties",
-        // Do not use `Object.defineProperty`.
-        { loose: true },
+        { loose: true }, // Do not use `Object.defineProperty`.
       ],
 
       [
         // See discussion in https://github.com/facebook/create-react-app/issues/4263
         "@babel/plugin-proposal-object-rest-spread",
-        // Use `Object.assign`.
-        { useBuiltIns: true },
+        { useBuiltIns: true }, // Use `Object.assign`.
       ],
 
-      [
-        "@babel/plugin-transform-runtime",
-        {
-          // Do not import `core-js`.
-          corejs: false,
+      isTest
+        ? null
+        : [
+            "@babel/plugin-transform-runtime",
+            {
+              // Do not import `core-js`.
+              corejs: false,
 
-          // Use `runtime` helpers.
-          helpers: true,
+              // Use `runtime` helpers.
+              helpers: true,
 
-          // Do not polyfill `regenerator`.
-          regenerator: false,
+              // Do not polyfill `regenerator`.
+              regenerator: false,
 
-          // Do not use `ES` modules.
-          useESModules: true,
-        },
-      ],
+              // Use es modules only for web.
+              useESModules: isWeb,
+            },
+          ],
     ]),
   };
 }
