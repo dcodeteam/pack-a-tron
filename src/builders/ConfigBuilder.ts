756301b398171e2619ddad836240529bd5f7d477
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-07 20:18:10 +0500

    feat(ConfigBuilder): Enable chunk splitting.

--- src/builders/ConfigBuilder.ts
+++ src/builders/ConfigBuilder.ts
@@ -192,10 +192,7 @@ export class ConfigBuilder extends AbstractConfigBuilder<Configuration> {
       // Automatically split vendor and commons
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-      // splitChunks: { // TODO: Enable.
-      //   chunks: "all",
-      //   name: false,
-      // },
+      splitChunks: this.isNode ? undefined : { chunks: "all", name: true },
     };
 
     this.minimizers = [new TerserPluginBuilder(options)];
