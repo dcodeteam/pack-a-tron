a43a6d45fe071c0e5d4b4d4e5afa887620391c2d
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-07 19:47:53 +0500

    feat(DevServerBuilder): Use `errorOverlayMiddleware` and `evalSourceMapMiddleware`.

--- src/builders/others/DevServerBuilder.ts
+++ src/builders/others/DevServerBuilder.ts
@@ -123,18 +123,16 @@ export class DevServerBuilder extends AbstractConfigBuilder<
         },
       },
 
-      // before(app) {
-      // This lets us fetch source contents from webpack for the error overlay
-      // app.use(evalSourceMapMiddleware(server));
-      // This lets us open files from the runtime error overlay.
-      // app.use(errorOverlayMiddleware());
-      // This service worker file is effectively a 'no-op' that will reset any
-      // previous service worker registered for the same host:port combination.
-      // We do this in development to avoid hitting the production cache if
-      // it used the same host and port.
-      // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      // app.use(noopServiceWorkerMiddleware());
-      // },
+      before: (app, ...args: unknown[]) => {
+        const errorOverlayMiddleware = require("react-dev-utils/errorOverlayMiddleware");
+        const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
+
+        // This lets us fetch source contents from webpack for the error overlay
+        app.use(evalSourceMapMiddleware(args[0]));
+
+        // This lets us open files from the runtime error overlay.
+        app.use(errorOverlayMiddleware());
+      },
     };
   }
 }
